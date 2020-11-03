a99c17b377995f27021bac9ad453881041e5d368
이용현 <dydwk231@naver.com>
2020-11-03 17:07:32 +0900

    using map & key

--- src/App.js
+++ src/App.js
@@ -4,20 +4,14 @@
 // import Say from "./Say";
 // import EventPractice from "./EventPractice";
 // import ValidationSample from "./ValidationSample";
+import IterationSample from "./IterationSample";
 
 import React, { Component } from "react";
 import ScrollBox from "./ScrollBox";
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <ScrollBox ref={(ref) => (this.ScrollBox = ref)} />
-        <button onClick={() => this.ScrollBox.scrollToBottom()}>
-          맨밑으로
-        </button>
-      </div>
-    );
+    return <IterationSample />;
   }
 }
 
--- src/IterationSample.js
+++ src/IterationSample.js
@@ -2,16 +2,8 @@ import React from "react";
 
 const IterationSample = () => {
   const names = ["눈사람", "얼음", "눈", "바람"];
-  const nameList = names.map((item) => <li>{item}</li>);
-  return (
-    <ul>{nameList}</ul>
-    // <ul>
-    //   <li>눈사람</li>
-    //   <li>얼음</li>
-    //   <li>눈</li>
-    //   <li>바람</li>
-    // </ul>
-  );
+  const nameList = names.map((item, index) => <li key={index}>{item}</li>);
+  return <ul>{nameList}</ul>;
 };
 
 export default IterationSample;
